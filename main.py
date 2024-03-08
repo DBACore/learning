8e675fb5a220a1a165e83a1a55ce283a93d20c31
Jorge Caceres <jorgecaceres.dbacore@latam.com>
2024-03-08 10:32:01 -0300

    Adding calendar

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# importing calendar module
+import calendar
+
+yy = 2024  # year
+mm = 3    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print("Calendario")
+print(calendar.month(yy, mm))
